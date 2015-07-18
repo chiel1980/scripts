e659fd96bdac87824e3865cf5a4fe60543fcfb43
Michiel van Es <mve@pragmasec.nl>
2015-07-19 00:15:19 +0200

    added scrot and feh - screenshot and image viewer

--- install-arch-laptop.sh
+++ install-arch-laptop.sh
@@ -6,7 +6,7 @@
 #
 ### Install packages
 #
-packages=(pulseaudio pulseaudio-alsa pa-applet-git offlineimap msmtp midori connman midori parcellite gnome-keyring synaptics xorg-server xorg-xinit i3 vlc libreoffice fish mutt curl lynx atom connman-ui-gtk terminator urxvt git ufw terminator chromium keepassx2 thunar-volman thunar-archive-plugin thunar-media-tags-plugin gvfs thunar gvfs filezilla clusterssh backintime-bzr thunar-volman thunar-archive-plugin thunar-media-tags-plugin gvfs davfs2 sshfs nfs-util pa-applet udisks2 udevil)
+packages=(pulseaudio pulseaudio-alsa pa-applet-git offlineimap msmtp midori connman midori parcellite gnome-keyring synaptics xorg-server xorg-xinit i3 vlc libreoffice fish mutt curl lynx atom connman-ui-gtk terminator urxvt git ufw terminator chromium keepassx2 thunar-volman thunar-archive-plugin thunar-media-tags-plugin gvfs thunar gvfs filezilla clusterssh backintime-bzr thunar-volman thunar-archive-plugin thunar-media-tags-plugin gvfs davfs2 sshfs nfs-util pa-applet udisks2 udevil feh scrot)
 dotfiles_url='https://github.com/chiel1980/dotfiles.git'
 function install_packages {
 pacman -Sy $packages
