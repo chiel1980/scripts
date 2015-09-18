a520567ebae3469dad7b0c9a9b15140bca5a07f0
Michiel van Es <mvanes@bol.com>
2015-09-18 15:50:58 +0200

    add get rootservers script for unbound dns resolver

---
+++ get-rootservers-for-unbound.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+###############
+ROOTFILE='/etc/unbound/root.hints'
+ROOTFILENEW='/etc/unbound/root.hints.new'
+BCKPFILE='/etc/unbound/root.hints.bck'
+DIFF='/tmp/root.hints.diff'
+#
+### make backup of old root.hints
+#
+if [ -f $ROOTFILE ]; then
+  echo 'ok we can continue'
+  cp -f $ROOTFILE $BCKPFILE
+else
+  echo 'seems the root.hint file is non existent - exit alert death!'
+  echo 'restoring backup now!'
+  mv $BCKPFILE $ROOTFILE
+  /etc/init.d/unbound restart
+  exit 1
+fi
+#
+### get new file
+#
+wget -q http://www.internic.net/domain/named.cache -O $ROOTFILENEW
+#
+### check if new file is not 0 bytes
+#
+if [ -s $ROOTFILENEW ]; then
+  echo 'seems the file has contents - check for diff'
+  diff $ROOTFILENEW $BCKPFILE > $DIFF
+        if [ -s $DIFF ]; then
+                echo 'seems to be a diff, move new file into place and restart unbound'
+                mv $ROOTFILENEW $ROOTFILE
+                /etc/init.d/unbound restart
+                exit 1
+        else
+                echo 'seems that the diff is empty'
+                echo 'no action needed'
+                exit 1
+        fi
+else
+  echo 'new hints file seems to be empty, exiting now and restoring backup'
+  mv $BCKPFILE $ROOTFILE
+  /etc/init.d/unbound restart
+fi
