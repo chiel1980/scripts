793a345b5ae5ad49f529518389b32c809d99f9df
Michiel van Es <mvanes@bol.com>
2015-09-22 16:38:00 +0200

    added fpg key validation for the root.hints file

--- get-rootservers-for-unbound.sh
+++ get-rootservers-for-unbound.sh
@@ -5,6 +5,7 @@ ROOTFILE='/etc/unbound/root.hints'
 ROOTFILENEW='/etc/unbound/root.hints.new'
 BCKPFILE='/etc/unbound/root.hints.bck'
 DIFF='/tmp/root.hints.diff'
+GPGKEY='/etc/unbound/named.cache.sig'
 #
 ### make backup of old root.hints
 #
@@ -22,22 +23,25 @@ fi
 ### get new file
 #
 wget -q http://www.internic.net/domain/named.cache -O $ROOTFILENEW
+wget -q http://www.internic.net/domain/named.cache.sig -O $GPGKEY
 #
 ### check if new file is not 0 bytes
 #
 if [ -s $ROOTFILENEW ]; then
   echo 'seems the file has contents - check for diff'
   diff $ROOTFILENEW $BCKPFILE > $DIFF
-        if [ -s $DIFF ]; then
-                echo 'seems to be a diff, move new file into place and restart unbound'
-                mv $ROOTFILENEW $ROOTFILE
-                /etc/init.d/unbound restart
-                exit 1
-        else
-                echo 'seems that the diff is empty'
-                echo 'no action needed'
-                exit 1
-        fi
+	if [ -s $DIFF ]; then
+  		echo 'seems to be a diff, move new file into place and restart unbound'
+		gpg --keyserver --recv-key 0xc1d27af9 1>/dev/null 2>&1
+ 	 	gpg --verify $GPGKEY $ROOTFILENEW
+  		mv $ROOTFILENEW $ROOTFILE
+  		/etc/init.d/unbound restart
+		exit 1
+	else
+  		echo 'seems that the diff is empty'
+  		echo 'no action needed'
+  		exit 1
+	fi
 else
   echo 'new hints file seems to be empty, exiting now and restoring backup'
   mv $BCKPFILE $ROOTFILE
