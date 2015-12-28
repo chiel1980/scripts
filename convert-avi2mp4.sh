cf290ae3dedcbfafb248d5ab2952dcbc3e7269da
Michiel Van Es <michielvanes@MvEs-MacBook-Pro.local>
2015-12-28 22:43:19 +0100

    small arguments fix for working audio

--- convert-avi2mp4.sh
+++ convert-avi2mp4.sh
@@ -7,4 +7,4 @@
 #do
 #    mv -v "$FILE" `echo $FILE | tr ' ' '_' `
 #done
-for i in `ls`;do avconv -i $i -c:v libx264 -c:a copy $i.mp4;done
+for i in `ls`;do avconv -i $i -vcodec libx264 -acodec libfaac $i.mp4;done
