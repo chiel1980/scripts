b4892f22a61cd577eb8b80600a4a8a76d9b98fce
Michiel van Es <chiel1980@users.noreply.github.com>
2026-01-01 19:16:50 +0100

    Update get-thatspecificsound.nl.sh

--- get-thatspecificsound.nl.sh
+++ get-thatspecificsound.nl.sh
@@ -50,6 +50,7 @@ if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 	find . -type f -print0 | LC_ALL=C xargs -0 sed -i'' -e '/Created/d'
 	find . -type f -print0 | LC_ALL=C xargs -0 sed -i'' -e '/window._tkq = window._tkq/d'
 	find . -type f -print0 | LC_ALL=C xargs -0 sed -i'' -e '/wpcom_marketing_bar_cta_click/d'
+	find . -type f -print0 | LC_ALL=C xargs -0 sed -i'' -e '/wpcom_marketing_bar_impression/d'
     # done sanitizing
 	echo ''
 	echo 'now copying the contents to our git repo..'
