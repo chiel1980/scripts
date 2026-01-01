f30d5d33887d1ff44c87ff6b5caf5149fecc685e
Michiel van Es <chiel1980@users.noreply.github.com>
2026-01-01 19:36:11 +0100

    Update get-thatspecificsound.nl.sh

--- get-thatspecificsound.nl.sh
+++ get-thatspecificsound.nl.sh
@@ -64,6 +64,7 @@ wget -q https://thatspecificsound.wordpress.com/ \
 	find . -type f -print0 | LC_ALL=C xargs -0 sed -i'' -e '/wpcom_marketing_bar_impression/d'
 	find . -type f -print0 | LC_ALL=C xargs -0 sed -i'' -e '/\#marketingbar/d'
 	find . -type f -print0 | LC_ALL=C xargs -0 sed -i'' -e '/link.addEventListener/d'
+	find . -type f -print0 | LC_ALL=C xargs -0 sed -i'' -e '/link\.addEventListener/d'
     # done sanitizing
 	echo ''
 	echo 'now copying the contents to our git repo..'
